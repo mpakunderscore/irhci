808d17770d0553aa37911c8514ec243c93cb3ce3
Pavel Kuzmin <mpak.spb@gmail.com>
2013-04-09 21:50:49 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Irhci::Application.config.secret_token = 'e66f93feda4e974db71253cbae287ff0805ad37c583b61da191f06a72d7881caebd69125be4d312ee10d7ed98f9874d7cc6020b365f156e8557ebc83d6c56c2a'
