8099c7705568738a4563106736bc801dd5ed0f0f
Pavel Kuzmin <mpak.spb@gmail.com>
2013-04-11 12:57:57 +0400

    alpha v0.3

--- app/controllers/upload_controller.rb
+++
@@ -1,9 +0,0 @@
-class UploadController < ApplicationController
-  def index
-    end
-  def upload_file
-      uploaded_file = params[:file]
-      file_content = uploaded_file.read
-      puts "file_content"
-    end
-end
