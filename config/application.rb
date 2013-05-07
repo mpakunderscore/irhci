38808223b0d4933a1799ddcda77e535ed4a02edd
Pavel Kuzmin <mpak.spb@gmail.com>
2013-05-07 20:35:07 +0400

    beta 0.7.6

--- config/application.rb
+++ config/application.rb
@@ -55,10 +55,10 @@ module Irhci
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    # config.assets.precompile = true
-    config.assets.precompile += %w( *.js )
+    config.assets.precompile += %w( labs.js )
     
-    config.serve_static_assets = true
+    # config.assets.precompile = true    
+    # config.serve_static_assets = true
     
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
